c52745f630e9780f266513054a6aff21a9727411
tclindner <tclindner@users.noreply.github.com>
2019-04-28 13:15:31 -0500

    Update tests.js

--- test/unit/tests.js
+++ test/unit/tests.js
@@ -1,13 +1,13 @@
-// eslint-disable-next-line no-var prefer-destructuring
+// eslint no-var: "off", prefer-destructuring: "off"
 var hexToRgb = window.hexToRgbJquery.hexToRgb;
 
-describe('hex-to-rgb tests', function() {
-  context('when a hex value is passed', function() {
-    it('with all six characters a valid corresponding rgb value should be returned', function() {
+describe('hex-to-rgb tests', () => {
+  describe('when a hex value is passed', () => {
+    it('with all six characters a valid corresponding rgb value should be returned', () => {
       hexToRgb('#f1f1f1').should.equal('rgb(241, 241, 241)');
     });
 
-    it('with three characters a valid corresponding rgb value should be returned', function() {
+    it('with three characters a valid corresponding rgb value should be returned', () => {
       hexToRgb('#fff').should.equal('rgb(255, 255, 255)');
     });
   });
